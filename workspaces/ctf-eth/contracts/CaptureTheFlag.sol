42a875251a928b29cb0600fc877a54395e7d1a69
Bogdan Sivochkin <b.sivochkin@digiu.ai>
2023-01-19 15:12:49 +0300

    Initialize mainnet_flashbots_branch

---
+++ workspaces/ctf-eth/contracts/CaptureTheFlag.sol
@@ -0,0 +1,26 @@
+/**
+ * SPDX-License-Identifier:MIT
+ */
+pragma solidity ^0.7.6;
+import "@opengsn/contracts/src/BaseRelayRecipient.sol";
+
+contract CaptureTheFlag is BaseRelayRecipient {
+
+    event FlagCaptured(address previousHolder, address currentHolder);
+
+    address public currentHolder = address(0);
+
+    function captureTheFlag() external {
+        address previousHolder = currentHolder;
+
+        currentHolder = _msgSender();
+
+        emit FlagCaptured(previousHolder, currentHolder);
+    }
+
+    constructor(address forwarder) {
+        trustedForwarder = forwarder;
+    }
+
+    string public override versionRecipient = "2.2.0";
+}
